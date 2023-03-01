a017ca8ade31bb19695e055c4f9d231316a06e5e
pavke02 <43935146+MarkoPavke@users.noreply.github.com>
2023-03-01 18:58:36 +0100

    Initial Project
    Initial project skeleton used from project that assistant gave to us.

---
+++ SIMS Booking/Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using SIMS_Booking.Model;
+using SIMS_Booking.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS_Booking.Repository
+{
+    public class UserRepository
+    {
+        private const string FilePath = "../../../Resources/Data/users.csv";
+
+        private readonly Serializer<User> _serializer;
+
+        private List<User> _users;
+
+        public UserRepository()
+        {
+            _serializer = new Serializer<User>();
+            _users = _serializer.FromCSV(FilePath);
+        }
+
+        public User GetByUsername(string username)
+        {
+            _users = _serializer.FromCSV(FilePath);
+            return _users.FirstOrDefault(u => u.Username == username);
+        }
+    }
+}
