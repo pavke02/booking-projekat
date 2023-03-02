0227b9bea76930cd4bc24bf094fcb998cfe694f9
pavke02 <43935146+MarkoPavke@users.noreply.github.com>
2023-03-02 11:00:00 +0100

    Owner View Initial
    Frontend done and implemented classes for countries and cities

---
+++ SIMS Booking/View/OwnerView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SIMS_Booking.Model;
+
+namespace SIMS_Booking.View
+{
+    /// <summary>
+    /// Interaction logic for OwnerView.xaml
+    /// </summary>
+    public partial class OwnerView : Window
+    {
+        public OwnerView()
+        {
+            InitializeComponent();
+            DataContext = new ComboBoxLists();
+        }
+    }
+}
