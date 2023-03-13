803c344b43572f9f59f5ad907f911786028cf0b3
pavke02 <43935146+MarkoPavke@users.noreply.github.com>
2023-03-13 22:08:37 +0100

    Full gallery implemented
    Tested and working

---
+++ SIMS Booking/View/Guest1GalleryView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SIMS_Booking.Model;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SIMS_Booking.View
+{
+    /// <summary>
+    /// Interaction logic for Guest1GalleryView.xaml
+    /// </summary>
+    public partial class Guest1GalleryView : Window
+    {
+        public Accommodation SelectedAccommodation { get; set; }          
+
+        public Guest1GalleryView(Accommodation selectedAccommodation)
+        {
+            InitializeComponent();
+            SelectedAccommodation = selectedAccommodation;
+            List<string> imageUrls = new List<string>();
+            List<string> imageUrls2 = new List<string>();
+
+            for (int i = 0; i < selectedAccommodation.ImageURLs.Count / 2; i++)
+            {
+                imageUrls.Add(selectedAccommodation.ImageURLs.ElementAt(i));
+            }
+            for (int i = selectedAccommodation.ImageURLs.Count/2; i < selectedAccommodation.ImageURLs.Count; i++)
+            {
+                imageUrls2.Add(selectedAccommodation.ImageURLs.ElementAt(i));
+            }
+
+            imageList.ItemsSource = imageUrls;
+            imageList2.ItemsSource = imageUrls2;
+
+
+        }
+    }
+}
--- SIMS Booking/View/Guest1MainView.xaml.cs
+++ SIMS Booking/View/Guest1MainView.xaml.cs
@@ -43,7 +43,7 @@ namespace SIMS_Booking.View
             UserReservations = new ObservableCollection<Reservation>(_reservationRepository.GetReservationsByUser(loggedUser.getID()));            
 
             _cityCountryRepository = cityCountryRepository;            
-            _reservedAccommodationRepository = reservedAccommodationRepository;                                               
+            _reservedAccommodationRepository = reservedAccommodationRepository;
         }
 
         private void AddFilters(object sender, RoutedEventArgs e)
@@ -66,7 +66,8 @@ namespace SIMS_Booking.View
 
         private void OpenGallery(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            Guest1GalleryView galleryView = new Guest1GalleryView(SelectedAccommodation);
+            galleryView.Show();
         }
 
         private void UpdateAccommodations(List<Accommodation> accommodations)
@@ -87,6 +88,28 @@ namespace SIMS_Booking.View
         {
             UpdateAccommodations(_accommodationRepository.GetAll());
             UpdateUserReservations(_reservationRepository.GetReservationsByUser(LoggedUser.getID()).ToList());
+        }
+
+        private void TabChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TabC.SelectedIndex == 1)
+            {
+                ReserveButton.IsEnabled = false;
+                ViewGalleryButton.IsEnabled = false;
+                AddFiltersButton.IsEnabled = false;
+            }
+            else if(TabC.SelectedIndex == 0 && DataGridAccommodations.SelectedIndex == -1)
+            {
+                ReserveButton.IsEnabled = false;
+                ViewGalleryButton.IsEnabled = false;
+                AddFiltersButton.IsEnabled = true;
+            }
+            else
+            {
+                ReserveButton.IsEnabled = true;
+                ViewGalleryButton.IsEnabled = true;
+                AddFiltersButton.IsEnabled = true;
+            }
         }
     }
 }
