89158151f2dee429a4617e220f3ba0bd025b7661
gagi155 <maslac.ra151.2020@gmail.com>
2023-04-15 14:40:20 +0200

    Update OwnerMainView.xaml.cs

--- SIMS Booking/UI/View/Owner/OwnerMainView.xaml.cs
+++ SIMS Booking/UI/View/Owner/OwnerMainView.xaml.cs
@@ -12,13 +12,9 @@ using SIMS_Booking.Service.RelationsService;
 
 namespace SIMS_Booking.UI.View.Owner
 {
-    public partial class OwnerMainView : UserControl, INotifyPropertyChanged
+    public partial class OwnerMainView : UserControl
     {        
         #region Property
-        public Dictionary<string, List<string>> Countries { get; set; }
-        public List<string> TypesCollection { get; set; }
-        public ObservableCollection<Accommodation> Accommodations { get; set; }
-        public ObservableCollection<Reservation> ReservedAccommodations { get; set; }
         public ObservableCollection<GuestReview> PastReservations { get; set; }
 
         public Reservation SelectedReservation { get; set; }
@@ -33,126 +29,7 @@ namespace SIMS_Booking.UI.View.Owner
         private readonly OwnerReviewService _ownerReviewService;
         private readonly PostponementService _postponementService;
         private readonly UserService _userService;
-
-        private string _accommodationName;
-        public string AccommodationName
-        {
-            get => _accommodationName;
-            set
-            {
-                if (value != _accommodationName)
-                {
-                    _accommodationName = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private string _city;
-        public string City
-        {
-            get => _city;
-            set
-            {
-                if (value != _city)
-                {
-                    _city = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private KeyValuePair<string, List<string>> _country;
-        public KeyValuePair<string, List<string>> Country
-        {
-            get => _country;
-            set
-            {
-                if (value.Key != _country.Key)
-                {
-                    _country = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private string _accommodationType;
-        public string AccommodationType
-        {
-            get => _accommodationType;
-            set
-            {
-                if (value != _accommodationType)
-                {
-                    _accommodationType = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private string _maxGuests;
-        public string MaxGuests
-        {
-            get => _maxGuests;
-            set
-            {
-                if (value != _maxGuests)
-                {
-                    _maxGuests = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private string _minReservationDays;
-        public string MinReservationDays
-        {
-            get => _minReservationDays;
-            set
-            {
-                if (value != _minReservationDays)
-                {
-                    _minReservationDays = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private string _cancelationPeriod;
-        public string CancelationPeriod
-        {
-            get => _cancelationPeriod;
-            set
-            {
-                if (value != _cancelationPeriod)
-                {
-                    _cancelationPeriod = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private string _imageURLs;
-        public string ImageURLs
-        {
-            get => _imageURLs;
-            set
-            {
-                if (value != _imageURLs)
-                {
-                    _imageURLs = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
         #endregion
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
 
         public OwnerMainView()
         {
