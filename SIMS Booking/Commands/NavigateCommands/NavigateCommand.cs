1e905d419fe83fbef57822fb248c4d3759c8857b
gagi155 <maslac.ra151.2020@gmail.com>
2023-04-22 16:06:06 +0200

    code refactoring

--- SIMS Booking/Commands/NavigateCommands/NavigateCommand.cs
+++ SIMS Booking/Commands/NavigateCommands/NavigateCommand.cs
@@ -10,27 +10,29 @@ namespace SIMS_Booking.Commands.NavigateCommands
     {
         private readonly INavigationService _navigationService;
         private readonly ViewModelBase _viewModel;
-        private readonly Func<bool> _condition;
+        private readonly Func<bool> _canExecuteCondition;
 
-        //ToDo: promeniti naziv condition
         //ToDO: Refaktorisati fajlove
-        //ToDo: napraviti naslednika navigateCommand with disable condition
-        public NavigateCommand(INavigationService navigationService, ViewModelBase viewModel = null, Func<bool> condition = null)
+        //ToDo: napraviti naslednika navigateCommand sa disable condition
+        public NavigateCommand(INavigationService navigationService)
         {
-            _viewModel = viewModel;
             _navigationService = navigationService;
-            _condition = condition;
+        }
 
-            //Question : da li je ovo okej, i da li bi moglo bolje da se uradi
-            if (_viewModel != null)
-                _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        public NavigateCommand(INavigationService navigationService, ViewModelBase viewModel, Func<bool> canExecuteCondition)
+        {
+            _navigationService = navigationService;
+            _viewModel = viewModel;
+            _canExecuteCondition = canExecuteCondition;
+
+            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
         }
 
         public override bool CanExecute(object? parameter)
         {
-            if (_condition == null) return base.CanExecute(parameter);
+            if (_canExecuteCondition == null) return base.CanExecute(parameter);
 
-            return _condition() && base.CanExecute(parameter);
+            return _canExecuteCondition() && base.CanExecute(parameter);
         }
 
         public override void Execute(object? parameter)
