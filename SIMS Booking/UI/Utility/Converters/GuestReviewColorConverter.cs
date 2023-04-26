5d3317285a1d02af7dedbb7f9d95c7724b7338dc
gagi155 <maslac.ra151.2020@gmail.com>
2023-04-26 21:33:33 +0200

    Created Converters file

--- SIMS Booking/UI/Utility/Converters/GuestReviewColorConverter.cs
+++ SIMS Booking/UI/Utility/Converters/GuestReviewColorConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System;
 using System.Windows.Data;
 
-namespace SIMS_Booking.UI.Utility.ColorConverters
+namespace SIMS_Booking.UI.Utility.Converters
 {
     internal class GuestReviewColorConverter : IValueConverter
     {
