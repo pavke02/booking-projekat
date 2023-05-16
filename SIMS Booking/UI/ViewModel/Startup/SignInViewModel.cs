191c74a9789a1eeb827b6e9469bf6a657184bad5
markok4 <markokrstic001@gmail.com>
2023-05-16 14:25:47 +0200

    Fixed bugs

--- SIMS Booking/UI/ViewModel/Startup/SignInViewModel.cs
+++ SIMS Booking/UI/ViewModel/Startup/SignInViewModel.cs
@@ -54,9 +54,8 @@ namespace SIMS_Booking.UI.ViewModel.Startup
 
         #endregion
 
-        private TourReview _tourReview;
-        private Tour _tour;
-        private MainWindowViewModel _mainViewModel;
+      
+        
 
         private readonly NavigationStore _navigationStore;
         private readonly ModalNavigationStore _modalNavigationStore;
@@ -223,7 +222,7 @@ namespace SIMS_Booking.UI.ViewModel.Startup
                     break;
                 case Roles.Guide:
                     _navigationStore.CurrentViewModel = new MainWindowViewModel(_tourService, _confirmTourService, _tourPointService,
-                        _textBox, _userService, _tourReview, _tourReviewService, _navigationStore, _modalNavigationStore, _mainViewModel,_tourRequestService);
+                        _textBox, _userService,  _tourReviewService, _navigationStore, _modalNavigationStore, _tourRequestService);
                     break;
             }
         }
