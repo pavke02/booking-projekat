94d3906c4b214debd328953df5c29faea2df009b
Zlajan <zlatkojovovic960@gmail.com>
2023-06-06 18:38:39 +0200

    Working on HCI features

---
+++ SIMS Booking/UI/ViewModel/Driver/DriverTutorialViewModel.cs
@@ -0,0 +1,41 @@
+﻿using SIMS_Booking.Commands.NavigateCommands;
+using SIMS_Booking.Service.NavigationService;
+using SIMS_Booking.UI.Utility;
+using SIMS_Booking.Utility.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SIMS_Booking.UI.ViewModel.Driver
+{
+    public class DriverTutorialViewModel : ViewModelBase
+    {
+        public ICommand NavigateBackCommand { get; }
+
+        private Uri _videoSource;
+        public Uri VideoSource
+        {
+            get { return _videoSource; }
+            set
+            {
+                _videoSource = value;
+                OnPropertyChanged(nameof(VideoSource));
+            }
+        }
+
+        public DriverTutorialViewModel(ModalNavigationStore modalNavigationStore)
+        {
+            NavigateBackCommand = new NavigateBackCommand(CreateCloseAddVehicleNavigationService(modalNavigationStore));
+
+            VideoSource = new Uri("../../../Resources/Videos/test123.mp4", UriKind.RelativeOrAbsolute);
+        }
+
+        private INavigationService CreateCloseAddVehicleNavigationService(ModalNavigationStore modalNavigationStore)
+        {
+            return new CloseModalNavigationService(modalNavigationStore);
+        }
+    }
+}
